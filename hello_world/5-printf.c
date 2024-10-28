e5020b4b3c5acb42b4add79bcd3b1753332c4a65
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-10-28 01:49:48 -0400

    puzzle complete

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - entry point
- * Result: Always 0
+ * @main printf
+ * Return: Always 0
 **/
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+return (0);
 }
