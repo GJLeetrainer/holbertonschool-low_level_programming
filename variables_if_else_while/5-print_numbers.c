687f5d8b769792729f9e6847f940faa25fd69a40
GJLeetrainer <jayleeannvelez@yahoo.com>
2024-10-28 02:10:31 -0400

    numbers game

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @main - Print every single digit base 10 number
+ * Return: Always 0
+**/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
