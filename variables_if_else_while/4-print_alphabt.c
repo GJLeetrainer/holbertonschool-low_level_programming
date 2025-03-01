471a5fbd2247a9ccdf09920672cd41e47ec93bec
GJLeetrainer <jayleeannvelez@yahoo.com>
2025-02-28 21:58:52 -0400

    hope

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+i#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
